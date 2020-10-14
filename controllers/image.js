8c0d49bb6f655067023d3f5f02ba9bd4d44b79f7
ariellinfy <ariellinfy@gmail.com>
2020-10-14 07:15:44 -0600

    adding files to github

---
+++ controllers/image.js
@@ -0,0 +1,41 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '2f1813404a2e49459c4173ce97feea37'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with API'))
+	}
+
+const handleImage = (req,res, db) => {
+	const { id } = req.body;
+	// let found = false;
+	// database.users.forEach(user => {
+	// 	if (user.id === id) {
+	// 		found = true;
+	// 		user.entries++
+	// 		return res.json(user.entries);
+	// 	}
+	// })
+	// if(!found) {
+	// 	res.status(400).json('user not found')
+	// }
+	db('users').where('id', '=', id)
+  	.increment('entries', 1)
+  	.returning('entries')
+  	.then(entries =>{
+  		res.json(entries[0]);
+  	})
+  	.catch(err=> res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+};
---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+//connecting database to server
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1', //localhost
+    user : 'postgres',
+    password : 'Infinite7*',
+    database : 'smartbrain'
+  }
+});
+
+// db.select('*').from('users').then(data => {
+// 	console.log(data);
+// });
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// const database = {
+// 	users: [
+// 		{
+// 			id:'123',
+// 			name: 'John',
+// 			password: 'cookies',
+// 			email: 'john@gmail.com',
+// 			entries: 0,
+// 			joined: new Date()
+// 		},
+// 				{
+// 			id:'124',
+// 			name: 'Sally',
+// 			password: 'bananas',
+// 			email: 'sally@gmail.com',
+// 			entries: 0,
+// 			joined: new Date()
+// 		},
+// 				{
+// 			id:'125',
+// 			name: 'Bob',
+// 			password: 'grapes',
+// 			email: 'bob@gmail.com',
+// 			entries: 0,
+// 			joined: new Date()
+// 		}
+// 	]
+// 	// login: [
+// 	// {
+// 	// 	id: '789',
+// 	// 	hash: '',
+// 	// 	email: 'john@gmail.com'
+// 	// }
+// 	// ]
+// }
+
+app.get('/', (req, res) => {res.send('success')})
+
+// app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', signin.handleSignin(db, bcrypt)) //works as well, see advanced JS function
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
+
+app.listen(3001, ()=>{
+	console.log('app is running on port 3001');
+})
+
+// / --> res = this is working
+// /signin --> POST = success/fail
+// /register --> POST = user
+// /profile/:userID --> GET = user
+// /image --> PUT = user
